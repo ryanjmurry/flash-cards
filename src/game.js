7b99dd8f1044ad23a09868e4679a93dfceba8815
Kelli Brooke and Ryan Murry <kelli@>
2018-08-06 11:48:48 -0700

    intial commit, question writing ew

---
+++ src/game.js
@@ -0,0 +1,8 @@
+import { Question } from './question.js';
+
+export class Game {
+  constructor() {
+
+  }
+
+}
---
+++ src/question.js
@@ -0,0 +1,28 @@
+export class Question {
+  constructor(question, answer) {
+    this.questions = {
+      "a step-by-step procedure to achieve a specific goal": "algorithm",
+      "a value that is passed into a function when it is called": "argument",
+      "a type of value that contains a sequence of other values": "array",
+      "the act of putting a value into a variable": "assignment",
+      "a mistake in a program": "bug",
+      "to run the code in a function": "call",
+      "a data type that can be defined by the programmer": "class",
+      "a program that converts code into an executable, and checks that the syntax is correct": "compiler",
+      "a variable that never changes its value": "constant",
+      "to cause a running program to stop due to an error": "crash",
+      "a value that contains other values": "data structure",
+      "to investigate and fix bugs": "debug",
+      "a float that can represent a wider range of numbers than a normal float": "double",
+      "synonym for run": "execute",
+      "a program, usually a single file, ready to be run": "executable",
+      "a type of value that represents numbers with fractional parts": "float",
+      "a piece of code that is not run until it is called": "function",
+      "synonym for object": "instance",
+      "to create an object from a class": "instantiate",
+      "a type of value that represents whole numbers": "integer",
+      
+    }
+
+  }
+}
